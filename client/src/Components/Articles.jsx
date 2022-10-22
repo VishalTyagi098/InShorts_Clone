618d5c8c0377cea08fedebd9c988a6bd5b9721aa
vishal <vishaltyagi.dev098@gmail.com>
2022-10-22 22:41:54 +0530

    initiated articles

---
+++ client/src/Components/Articles.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import {useEffect} from 'react';
+import {Box} from '@mui/material';
+
+function Articles() {
+
+  useEffect(()=>{
+
+  },[]);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Articles
