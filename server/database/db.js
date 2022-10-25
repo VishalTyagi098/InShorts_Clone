7e453183bf95a97fa5083fd327ca779f380197a5
vishal <vishaltyagi.dev098@gmail.com>
2022-10-26 01:38:39 +0530

    connecting db

--- server/database/db.js
+++ server/database/db.js
@@ -3,12 +3,17 @@ import mongoose from 'mongoose'
 
 
 const Connection=async()=>{
-  const URL='';
+
+  const URL=`mongodb://vishal:vishalvinu@ac-tay7apc-shard-00-00.47xyb8y.mongodb.net:27017,ac-tay7apc-shard-00-01.47xyb8y.mongodb.net:27017,ac-tay7apc-shard-00-02.47xyb8y.mongodb.net:27017/?ssl=true&replicaSet=atlas-93m5pu-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  
   try{
 
     mongoose.connect(URL,{useNewUrlParser:true});
+    console.log("Database connected succesfully");
 
   }catch(error){
     console.log("Error while connecting to databse: ",error);
   }
 }
+
+export default Connection;
