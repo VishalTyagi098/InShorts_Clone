a349dafb22f4fdd79020993b06d00eaaaa60c194
vishal <vishaltyagi.dev098@gmail.com>
2022-10-26 01:24:00 +0530

    small backend change

--- server/index.js
+++ server/index.js
@@ -4,4 +4,4 @@ const app=express();
 
 
 const PORT=8000;
-app.listen(PORT,()=>console.log(`Server started successfully at port ${PORT}`));
+app.listen(PORT,()=>console.log(`Server is running successfully at port ${PORT}`));
