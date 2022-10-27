1bd00b352c981d9ae14febcbe3d3a815cf2f9dd8
vishal <vishaltyagi.dev098@gmail.com>
2022-10-27 16:19:26 +0530

    comments

--- server/index.js
+++ server/index.js
@@ -7,6 +7,9 @@ const app=express();
 
 const PORT=8000;
 
+// connects backend to db
 Connection();
+// starts the backend server
 app.listen(PORT,()=>console.log(`Server is running successfully at port ${PORT}`));
+// sends default data to db
 defaultData();
