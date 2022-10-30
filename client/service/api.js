06ca6654c1807db26fa5798eea6608a6954d6e39
vishal <vishaltyagi.dev098@gmail.com>
2022-10-30 18:34:07 +0530

    adding comments

--- client/service/api.js
+++ client/service/api.js
@@ -1,10 +1,16 @@
 import axios from 'axios'
 
+// This function will go to the localhost:8000/news and GET the data from backend. This function is then imported into the Articles.jsx component to be used
+
+// So, this api.js is used to get different data from different API endpoints in the form of functions. These functions can be used to get data and use them by importing them into seperate components
+
+// So, we require backend to provide this data and we will create different endpoints inside the 'controllers' folder
 export const getNews=async()=>{
-  const URL='';
+  // URL of the backend server NOT the frontend server
+  const URL='http://localhost:8000';
 
   try{
-    await axios.get(URL);
+    return await axios.get(`${URL}/news`);
   }catch(error){
     console.log("Error while calling getNews api: ",error);
   }
