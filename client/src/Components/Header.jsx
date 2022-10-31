0e9db36845fad769441fab7ea6bb09ec2ac2e723
vishal <vishaltyagi.dev098@gmail.com>
2022-10-31 13:09:49 +0530

    installed tailwind

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -7,6 +7,9 @@ export default function Header() {
     <div sx={{ flexGrow: 1 }}>
       <div position="static">
         <div>
+        <h1 className="text-5xl font-bold underline">
+          Hello world!
+        </h1>
           
             <img src="https://assets.inshorts.com/website_assets/images/logo_inshorts.png" alt="" />
         </div>
