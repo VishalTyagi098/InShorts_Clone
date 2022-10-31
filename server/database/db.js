fb2e6eb3734258e1f3bcfb75b0cc7d9b9b51f4ba
vishal <vishaltyagi.dev098@gmail.com>
2022-10-31 10:57:48 +0530

    .env used

--- server/database/db.js
+++ server/database/db.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose'
 
 
 
-const Connection=async()=>{
+const Connection=async(username,password)=>{
 
-  const URL=`mongodb://vishal:vishalvinu@ac-tay7apc-shard-00-00.47xyb8y.mongodb.net:27017,ac-tay7apc-shard-00-01.47xyb8y.mongodb.net:27017,ac-tay7apc-shard-00-02.47xyb8y.mongodb.net:27017/?ssl=true&replicaSet=atlas-93m5pu-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  const URL=`mongodb://${username}:${password}@ac-tay7apc-shard-00-00.47xyb8y.mongodb.net:27017,ac-tay7apc-shard-00-01.47xyb8y.mongodb.net:27017,ac-tay7apc-shard-00-02.47xyb8y.mongodb.net:27017/?ssl=true&replicaSet=atlas-93m5pu-shard-0&authSource=admin&retryWrites=true&w=majority`;
   
   try{
 
--- server/index.js
+++ server/index.js
@@ -3,8 +3,13 @@ import Connection from './database/db.js';
 import defaultData from './default.js';
 import Route from './routes/Route.js';
 import cors from 'cors'
+import dotenv from 'dotenv'
 
 const app=express();
+// .env work
+dotenv.config();
+const username=process.env.DB_USERNAME;
+const password=process.env.DB_PASSWORD;
 
 app.use(cors())
 // 4. calling routes
@@ -13,7 +18,7 @@ app.use('/',Route);
 const PORT=8000;
 
 // 1. connects backend to db
-Connection();
+Connection(username,password);
 // 2. starts the backend server
 app.listen(PORT,()=>console.log(`Server is running successfully at port ${PORT}`));
 // 3. sends default data to db
