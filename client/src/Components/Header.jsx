d315228c367680ab6c973e5753675fc15ae87a4a
vishal <vishaltyagi.dev098@gmail.com>
2022-10-31 14:02:48 +0530

    working on navbar

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,19 +1,21 @@
 import * as React from 'react';
-
+import menuIcon from '../images/menuIcon.png'
 
 
 export default function Header() {
   return (
-    <div sx={{ flexGrow: 1 }}>
-      <div position="static">
-        <div>
-        <h1 className="text-5xl font-bold underline">
-          Hello world!
-        </h1>
-          
-            <img src="https://assets.inshorts.com/website_assets/images/logo_inshorts.png" alt="" />
-        </div>
+    <div className='w-full flex justify-center items-center h-[72px] relative shadow-[0_2px_5px_0px_rgba(0,0,0,0.16)] '>
+
+      <div className="mr-auto ml-5">
+        <img src={menuIcon} alt="" width={30}/>
+      </div>
+
+      <div className="h-[50px] absolute">
+        <a href="https://www.inshorts.com/">
+          <img src="https://assets.inshorts.com/website_assets/images/logo_inshorts.png" alt="" width={140}/>
+        </a>
       </div>
     </div>
+      
   );
 }
